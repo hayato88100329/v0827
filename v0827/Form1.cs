0c412a1d60adf14c33edfe723f20e1c7faac9820
nozawa <DASchool.12@gmail.com>
2020-08-27 09:42:22 +0900

    完成

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,9 +12,24 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Left += vx;
+            label1.Top += vx;
         }
     }
 }
